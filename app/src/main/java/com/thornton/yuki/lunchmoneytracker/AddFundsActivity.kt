caa21195dd9892663ae756dedf4538a96ef9807d
YukiThornton <16602330+YukiThornton@users.noreply.github.com>
2018-12-12 21:01:13 +0900

    Added 'Add Funds' functionality

--- app/src/main/java/com/thornton/yuki/lunchmoneytracker/AddFundsActivity.kt
+++ app/src/main/java/com/thornton/yuki/lunchmoneytracker/AddFundsActivity.kt
@@ -1,44 +1,66 @@
 package com.thornton.yuki.lunchmoneytracker
 
+import android.app.Activity
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.KeyEvent
 import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
 import android.widget.EditText
+import android.widget.LinearLayout
 import android.widget.TextView
 
 class AddFundsActivity : AppCompatActivity() {
 
-    private val tag = "DEV ADD FUNDS ACTIVITY"
-    private var currentBalance: Int = -1
+    private val tag = "LUNCH_DEV_ADD_FUNDS_ACT"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_funds)
 
-        currentBalance = intent.getIntExtra(INTENT_KEY_CURRENT_BALANCE, -1)
+        val currentBalance = intent.getIntExtra(INTENT_KEY_CURRENT_BALANCE, -1)
         findViewById<EditText>(R.id.new_balance).apply {
             setText(currentBalance.toString(), TextView.BufferType.EDITABLE)
             setOnKeyListener(View.OnKeyListener { v, keyCode, event ->
                 if (keyCode == KeyEvent.KEYCODE_ENTER) {
-                    startMainActivity()
+                    changeBalanceAndReturnMain()
                     return@OnKeyListener true
                 }
                 false
             })
         }
+        val fixedAmounts = arrayOf(2000, 2100, 2200)
+        val buttonIds = arrayOf(R.id.fixed_amount_button1, R.id.fixed_amount_button2, R.id.fixed_amount_button3)
+        for ((index, buttonId) in buttonIds.withIndex()) {
+            findViewById<Button>(buttonId).apply {
+                setText("+ ¥${fixedAmounts[index]}", TextView.BufferType.NORMAL)
+                setOnClickListener{ addFundsAndReturnMain(fixedAmounts[index]) }
+            }
+        }
     }
 
-    private fun startMainActivity() {
-        Log.d(tag, "Going back to MainActivity")
-        currentBalance = Integer.parseInt(findViewById<EditText>(R.id.new_balance).text.toString())
-        val intent = Intent(this, MainActivity::class.java).apply {
-            addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP)
-            putExtra(INTENT_KEY_CURRENT_BALANCE, currentBalance)
+    private fun addFundsAndReturnMain(amount: Int) {
+        Log.d(tag, "addFundsAndReturnMain: funds amount=$amount")
+        returnMain(getBalanceFromInput() + amount)
+    }
+
+    private fun changeBalanceAndReturnMain() {
+        returnMain(getBalanceFromInput())
+    }
+
+    private fun getBalanceFromInput() :Int {
+        return Integer.parseInt(findViewById<EditText>(R.id.new_balance).text.toString())
+    }
+
+    private fun returnMain(finalBalance: Int) {
+        Log.d(tag, "returnMain: finalBalance=$finalBalance")
+        val intent = Intent().apply {
+            putExtra(INTENT_KEY_CURRENT_BALANCE, finalBalance)
         }
-        startActivity(intent)
+        setResult(Activity.RESULT_OK, intent)
         finish()
     }
 }
--- app/src/main/java/com/thornton/yuki/lunchmoneytracker/MainActivity.kt
+++ app/src/main/java/com/thornton/yuki/lunchmoneytracker/MainActivity.kt
@@ -1,41 +1,22 @@
 package com.thornton.yuki.lunchmoneytracker
 
+import android.app.Activity
 import android.content.Intent
-import android.content.SharedPreferences
 import android.os.Bundle
 import android.preference.PreferenceManager
-import android.support.design.widget.BottomNavigationView
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
 import android.view.View
 import android.widget.TextView
-import kotlinx.android.synthetic.main.activity_main.*
 
 const val INTENT_KEY_CURRENT_BALANCE = "CURRENT_BALANCE"
 const val PREF_KEY_CURRENT_BALANCE = "CURRENT_BALANCE"
 
 class MainActivity : AppCompatActivity() {
 
-    private val tag = "DEV MAIN ACTIVITY"
+    private val tag = "LUNCH_DEV_MAIN_ACT"
+    private val activityCode = 1
     private var currentBalance: Int = -1
-
-    private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
-        when (item.itemId) {
-            R.id.navigation_home -> {
-                message.setText(R.string.title_home)
-                return@OnNavigationItemSelectedListener true
-            }
-            R.id.navigation_dashboard -> {
-                message.setText(R.string.title_dashboard)
-                return@OnNavigationItemSelectedListener true
-            }
-            R.id.navigation_notifications -> {
-                message.setText(R.string.title_notifications)
-                return@OnNavigationItemSelectedListener true
-            }
-        }
-        false
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         Log.d(tag, "onCreate")
@@ -45,32 +26,35 @@ class MainActivity : AppCompatActivity() {
         val prefs = PreferenceManager.getDefaultSharedPreferences(this)
         val balance = prefs.getInt(PREF_KEY_CURRENT_BALANCE, resources.getInteger(R.integer.initial_funds))
         setBalance(balance)
-
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
     }
 
     private fun setBalance(balance: Int) {
+        Log.d(tag, "setBalance: value=$balance")
         currentBalance = balance
         val currentBalanceTextView = findViewById<TextView>(R.id.balance)
         currentBalanceTextView.text = currentBalance.toString()
     }
 
     fun onAddFundsClicked(view: View) {
-        Log.d(tag, "Add Funds Clicked")
+        Log.d(tag, "onAddFundsClicked")
         startAddFundsActivity()
     }
 
     private fun startAddFundsActivity() {
+        Log.d(tag, "startAddFundsActivity")
         val intent = Intent(this, AddFundsActivity::class.java).apply {
             putExtra(INTENT_KEY_CURRENT_BALANCE, currentBalance)
         }
-        startActivity(intent)
+        startActivityForResult(intent, activityCode)
     }
 
-    override fun onNewIntent(intent: Intent?) {
-        super.onNewIntent(intent)
-        val safeIntent = intent ?: getIntent()
-        setBalance(safeIntent.getIntExtra(INTENT_KEY_CURRENT_BALANCE, currentBalance))
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        Log.d(tag, "onActivityResult")
+        if (requestCode == activityCode && resultCode == Activity.RESULT_OK) {
+            val safeIntent = data ?: intent
+            setBalance(safeIntent.getIntExtra(INTENT_KEY_CURRENT_BALANCE, currentBalance))
+        }
     }
 
     override fun onStop() {
